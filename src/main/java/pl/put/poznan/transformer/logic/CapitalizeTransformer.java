1cefe21569d326eaf3ecc1d370d1a8ad02675825
MBernart <mateuszbernart@gmail.com>
2024-05-11 13:24:09 +0200

    Add CapitalizeTransformer transformers

---
+++ src/main/java/pl/put/poznan/transformer/logic/CapitalizeTransformer.java
@@ -0,0 +1,25 @@
+package pl.put.poznan.transformer.logic;
+
+public class CapitalizeTransformer implements TextTransformer
+{
+    @Override
+    public String transform(String text)
+    {
+        String[] words = text.split(" ");
+
+        StringBuilder result = new StringBuilder();
+
+        for (String word : words)
+        {
+            if (word.isEmpty())
+                continue;
+
+            result.append(Character.toUpperCase(word.charAt(0)))
+                    .append(word.substring(1))
+                    .append(" ");
+
+        }
+
+        return result.toString().trim();
+    }
+}
--- src/main/java/pl/put/poznan/transformer/logic/TextTransformer.java
+++ src/main/java/pl/put/poznan/transformer/logic/TextTransformer.java
@@ -3,9 +3,7 @@ package pl.put.poznan.transformer.logic;
 /**
  * This is just an example to show that the logic should be outside the REST service.
  */
-public class TextTransformer {
-    public String transform(String text){
-        // of course, normally it would do something based on the transforms
-        return text.toUpperCase();
-    }
+public interface TextTransformer
+{
+    public String transform(String text);
 }
