0e82a448cdff55f43388b86cc654187f216f144c
Patryk Janiak <xmorswinx@gmail.com>
2024-05-12 14:11:06 +0200

    Add unit tests for repetition and reverse transformers

---
+++ src/test/java/pl/put/poznan/transformer/logic/RemoveRepetitionTest.java
@@ -0,0 +1,25 @@
+package pl.put.poznan.transformer.logic;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RemoveRepetitionTest {
+    private static RemoveRepetition transformer;
+
+    @BeforeAll
+    static void setUp() {
+        transformer = new RemoveRepetition();
+    }
+
+    @Test
+    void testExample() {
+        assertEquals("Send me a message", transformer.transform("Send me me a message"));
+    }
+
+    @Test
+    void testTricky() {
+        assertEquals("jest.", transformer.transform("jest: jest! jest.")); // TODO: Decide whether this behavior is desired
+    }
+}
