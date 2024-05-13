4e62f8efb08878264fb27c15ff7e146328865ab8
MBernart <mateuszbernart@gmail.com>
2024-05-13 02:16:52 +0200

    Add javadoc for lower, upper, and TextTransformer interface

--- src/main/java/pl/put/poznan/transformer/logic/LowercaseTransformer.java
+++ src/main/java/pl/put/poznan/transformer/logic/LowercaseTransformer.java
@@ -1,7 +1,15 @@
 package pl.put.poznan.transformer.logic;
 
+/**
+ * This class is responsible for transforming text to lowercase.
+ */
 public class LowercaseTransformer implements TextTransformer
 {
+    /**
+     * This method transforms the input text to lowercase.
+     * @param text The input text to be transformed.
+     * @return The transformed text.
+     */
     @Override
     public String transform(String text)
     {
--- src/main/java/pl/put/poznan/transformer/logic/TextTransformer.java
+++ src/main/java/pl/put/poznan/transformer/logic/TextTransformer.java
@@ -1,9 +1,23 @@
 package pl.put.poznan.transformer.logic;
 
 /**
- * This is just an example to show that the logic should be outside the REST service.
+ * This interface is used to define the contract for text transformers.
+ * Each text transformer should implement this interface.
+ * The transform method should take a string as an argument and return a transformed string.
+ * The transformation should be defined by the implementing class.
+ * The transform method should not modify the original string.
+ * The transform method should return a new string with the transformation applied.
+ * The transform method should not return null.
+ * The transform method should return an empty string if the input string is empty.
+ * The transform method should not return a string with leading or trailing whitespaces.
  */
 public interface TextTransformer
 {
+    /**
+     * This method should take a string as an argument and return a transformed string.
+     * The transformation should be defined by the implementing class.
+     * @param text The input string to be transformed.
+     * @return The transformed string.
+     */
     public String transform(String text);
 }
