7f4ad1e68bd450b295400ca07ea105825775f4cc
Strajkerr <71469217+Strajkerr@users.noreply.github.com>
2024-05-25 21:05:11 +0200

    Upload mock tests

---
+++ src/test/java/pl/put/poznan/transformer/logic/TextTransformerServiceTest.java
@@ -0,0 +1,98 @@
+package pl.put.poznan.transformer.logic;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+class TextTransformerServiceTest {
+    @Test
+    public void testTransform() {
+        NumberTransformer mockNumber = mock(NumberTransformer.class);
+        TextTransformerService mockService = mock(TextTransformerService.class);
+        List<String> operations = new ArrayList<>();
+        operations.add("number");
+        when(mockNumber.transform("12")).thenReturn("twelve");
+        when(mockService.transform(anyString(), eq(operations))).thenAnswer(
+                new Answer() {
+                    public Object answer(InvocationOnMock invocationOnMock) {
+                        Object[] args = invocationOnMock.getArguments();
+                        Object mock = invocationOnMock.getMock();
+                        return mockNumber.transform(args[0].toString());
+                    }
+        });
+        Assertions.assertEquals("twelve", mockService.transform("12", operations));
+    }
+    @Test
+    public void testUppercase() {
+        UppercaseTransformer mockUppercase = mock(UppercaseTransformer.class);
+        TextTransformerService mockService = mock(TextTransformerService.class);
+        List<String> operations = new ArrayList<>();
+        operations.add("upper");
+        when(mockUppercase.transform("okay")).thenReturn("OKAY");
+        when(mockService.transform(anyString(), eq(operations))).thenAnswer(
+                new Answer() {
+                    public Object answer(InvocationOnMock invocationOnMock) {
+                        Object[] args = invocationOnMock.getArguments();
+                        Object mock = invocationOnMock.getMock();
+                        return mockUppercase.transform(args[0].toString());
+                    }
+                });
+        Assertions.assertEquals("OKAY", mockService.transform("okay", operations));
+    }
+    @Test
+    public void testCapitalize() {
+        CapitalizeTransformer mockCapitalize = mock(CapitalizeTransformer.class);
+        TextTransformerService mockService = mock(TextTransformerService.class);
+        List<String> operations = new ArrayList<>();
+        operations.add("capitalize");
+        when(mockCapitalize.transform("okay")).thenReturn("Okay");
+        when(mockService.transform(anyString(), eq(operations))).thenAnswer(
+                new Answer() {
+                    public Object answer(InvocationOnMock invocationOnMock) {
+                        Object[] args = invocationOnMock.getArguments();
+                        Object mock = invocationOnMock.getMock();
+                        return mockCapitalize.transform(args[0].toString());
+                    }
+                });
+        Assertions.assertEquals("Okay", mockService.transform("okay", operations));
+    }
+    @Test
+    public void testLowercase() {
+        LowercaseTransformer mockLowercase = mock(LowercaseTransformer.class);
+        TextTransformerService mockService = mock(TextTransformerService.class);
+        List<String> operations = new ArrayList<>();
+        operations.add("upper");
+        when(mockLowercase.transform("OKAY")).thenReturn("okay");
+        when(mockService.transform(anyString(), eq(operations))).thenAnswer(
+                new Answer() {
+                    public Object answer(InvocationOnMock invocationOnMock) {
+                        Object[] args = invocationOnMock.getArguments();
+                        Object mock = invocationOnMock.getMock();
+                        return mockLowercase.transform(args[0].toString());
+                    }
+                });
+        Assertions.assertEquals("okay", mockService.transform("OKAY", operations));
+    }
+    @Test
+    public void testReverse() {
+        ReverseTransformer mockReverse = mock(ReverseTransformer.class);
+        TextTransformerService mockService = mock(TextTransformerService.class);
+        List<String> operations = new ArrayList<>();
+        operations.add("upper");
+        when(mockReverse.transform("MirEk")).thenReturn("KerIm");
+        when(mockService.transform(anyString(), eq(operations))).thenAnswer(
+                new Answer() {
+                    public Object answer(InvocationOnMock invocationOnMock) {
+                        Object[] args = invocationOnMock.getArguments();
+                        Object mock = invocationOnMock.getMock();
+                        return mockReverse.transform(args[0].toString());
+                    }
+                });
+        Assertions.assertEquals("KerIm", mockService.transform("MirEk", operations));
+    }
+}
